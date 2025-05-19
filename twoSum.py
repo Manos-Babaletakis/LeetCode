46b0ed8bbc96c706d0d5bd90c02aff3d75730754
Manos Babaletakis <emmbamp@gmail.com>
2025-05-19 11:30:54 +0200

    twoSum_1

---
+++ twoSum.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv, dotenv_values 
+import os
+import numpy as np
+# loading variables from .env file
+load_dotenv()
+local_path = os.getenv("LOCAL_PATH")
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for
+        
+sol = Solution([3,6,7,2,12,15,22], 29)
+
+print(sol.twoSum)
