57f7feaf6623ed7f06a6eedf26340f376e725f20
Manos Babaletakis <emmbamp@gmail.com>
2025-05-21 01:11:15 +0200

    4. O(m+n)

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -14,14 +14,36 @@ class Solution(object):
         m_max = len(nums1)
         n_max = len(nums2)
         
-        m = m_max//2 + m_max%2
-        n = n_max//2 + n_max%2
+        placeholder_1 = 0
+        placeholder_2 = 0
         
-        if (m_max+n_max)%2 == 1:
-            
+        m=0
+        n=0
+        
+        for _ in range(0, (m_max + n_max)//2 + 1):
+            placeholder_2 = placeholder_1
+            if m < m_max and n < n_max:
+                if nums1[m] < nums2[n]:
+                    placeholder_1 = nums1[m]
+                    m += 1
+                else:
+                    placeholder_1 = nums2[n]
+                    n += 1
+            elif m < m_max:
+                placeholder_1 = nums1[m]
+                m += 1
+            else:
+                placeholder_1 = nums2[n]
+                n += 1
+        
+        if (m_max + n_max)%2 == 1:
+            return placeholder_1
+        else:
+            return (placeholder_1 + placeholder_2)/2
             
 list_1 = [1, 10, 35]
-list_2 = [1, 2, 5, 8, 14, 35, 78, 79, 80, 80.5, 81, 99, 101]
+list_2 = [1, 2, 5, 8, 14, 35.5, 78, 79, 80, 80.5, 81, 99, 101]
+output=35.25
 
 sol = Solution()
-print(sol.lengthOfLongestSubstring(total_string) == output)
+print(sol.findMedianSortedArrays(list_1, list_2) == output)
