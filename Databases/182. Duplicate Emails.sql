c18c37a695f2ad00e6609cc7b505da2f825738d6
Manos Babaletakis <emmbamp@gmail.com>
2025-05-21 05:27:10 +0200

    2nd way for 182

--- Databases/182. Duplicate Emails.sql
+++ Databases/182. Duplicate Emails.sql
@@ -3,4 +3,10 @@ SELECT DISTINCT(P1.email) AS Email
 FROM Person P1 INNER JOIN
      Person P2 ON
      P1.email = P2.email
-WHERE P1.id != P2.id
+WHERE P1.id != P2.id;
+
+#2nd way
+SELECT email AS Email
+FROM Person
+group by email
+having count(email) > 1;
