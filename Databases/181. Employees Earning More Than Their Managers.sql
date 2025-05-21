cd63c7227e27ea1bed639221c5db9d4c3d03731c
Manos Babaletakis <emmbamp@gmail.com>
2025-05-21 05:19:52 +0200

    Completed 181

---
+++ Databases/181. Employees Earning More Than Their Managers.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT E1.name AS Employee
+FROM Employee E1 JOIN
+     Employee E2 ON
+    E1.managerId = E2.id
+WHERE E1.managerId IS NOT NULL AND
+    E1.salary > E2.salary
