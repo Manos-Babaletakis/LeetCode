f8a5729b412b03a07470462dc0a4743f134b1ae0
Manos Babaletakis <emmbamp@gmail.com>
2025-05-25 18:05:25 +0200

    184 Completed

---
+++ Databases/184. Department Highest Salary.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT D.name   AS Department,
+       E.name   AS Employee,
+       E.salary AS Salary
+FROM Employee E
+INNER JOIN Department D
+ON E.departmentId = D.id
+WHERE (D.id, E.salary) IN 
+    (SELECT departmentId, max(salary)
+     FROM Employee
+     GROUP BY departmentId);
