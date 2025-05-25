987ef3e0607f0e53bd9d5946d0e2c042cc1cf63c
Manos Babaletakis <emmbamp@gmail.com>
2025-05-25 16:53:19 +0200

    183 2nd solution

--- Databases/183. Customers Who Never Order.sql
+++ Databases/183. Customers Who Never Order.sql
@@ -3,3 +3,10 @@ Select C.name Customers
 From Customers C
 WHERE C.id NOT IN (SELECT DISTINCT(customerId)
                    FROM Orders);
+
+-- solution 2
+Select C.name Customers
+From Customers C
+LEFT JOIN Orders O ON
+C.id = O.customerId
+WHERE O.customerId IS NULL;
