210dd64d7c427e22285d495066e1317ba2914e03
Manos Babaletakis <emmbamp@gmail.com>
2025-05-26 04:17:34 +0200

    183 2nd solution

--- Databases/184. Department Highest Salary.sql
+++ Databases/184. Department Highest Salary.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+-- Write your MySQL query statement below
 SELECT D.name   AS Department,
        E.name   AS Employee,
        E.salary AS Salary
@@ -9,3 +9,25 @@ WHERE (D.id, E.salary) IN
     (SELECT departmentId, max(salary)
      FROM Employee
      GROUP BY departmentId);
+
+-- solution 2
+WITH ranked_salaries AS(
+    SELECT 
+        salary,
+        name,
+        departmentId,
+        DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS salary_rank
+    FROM Employee  
+)
+
+SELECT
+    d.name AS Department,
+    r.name AS Employee,
+    r.salary AS Salary
+FROM
+    ranked_salaries r
+JOIN
+    Department d
+ON
+    d.id = r.departmentId
+WHERE r.salary_rank = 1;
