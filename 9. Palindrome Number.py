b78c5ba5df7ee815c78785db0352cc44871977de
Manos Babaletakis <emmbamp@gmail.com>
2025-05-29 18:43:26 +0200

    9. 5th solution

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -15,17 +15,19 @@ class Solution(object):
             k+=1
         return True
     
+    
+    
     def isPalindrome_complex(self, x):
         return str(x) == ''.join(reversed(str(x)))
+    
+    
     
     def isPalindrome_simple(self, x):
         return str(x) == str(x)[::-1]
     
+    
+    
     def isPalindrome_4(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
         if x<0 or (x%10==0 and x!=0):
             return False
 
@@ -39,3 +41,16 @@ class Solution(object):
                 k+=1
             except:
                 return True
+            
+            
+            
+    def isPalindrome_numerical(self, x):
+        if x<0 or (x%10==0 and x!=0):
+            return False
+
+        original = x
+        reverse = 0
+        while x>0:
+            reverse = reverse*10 + x%10
+            x= x//10
+        return original == reverse
